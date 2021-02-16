0e61ebca670619b7dbdf3386f00bf5fe4e787de2
Barvinko <vlad.barvinko@gmail.com>
2021-02-16 22:48:59 +0200

    finished distribution

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,43 +7,56 @@ function translation(){
     let wordNumbers00 = [" ", "десять", "дватцять","тридцять","сорок","пятдесять","шисдесят","сімдесят","вісімдесят","девяносто"];
     let wordNumbers000 = ["","сто", "двісті", "триста","чотириста","пятсот","шістсот","сімсот","вісімсот","девятсот"];
     let result = new Array();
-    for (let i = 0; i < 3; i++) {
+    let howManyFullTrees = Math.trunc((numbers.length -1)/3);
+    console.log(howManyFullTrees);
+    howManyNumbers = new Array;
+     for (let i = 0; i < howManyFullTrees + 1; i++) {
+        if (howManyFullTrees == (numbers.length)/3 || i < howManyFullTrees) {
+            howManyNumbers[i] = 3;
+        }else {
+            howManyNumbers[i] = numbers.length - (howManyFullTrees * 3)
+        }
+    }
+    console.log(howManyNumbers);
+
+    for (let i = 0; i < howManyFullTrees + 1; i++) {
         result[i] = new Array();
+
+        for (let j = 0; j < howManyNumbers[i]; j++) {
+            result[i][j] = numbers[j + i*3];
+            console.log(`numbers[j] ${numbers[j]} j: ${j}`);
+        }
     }
     console.log(result);
     
 
-for (let j = 0; j < Math.ceil(numbers.length/3); j++) {
-    let i = 0;
-    let minusBeforeNum = 0;
-    if (j >= 1) {
-        i += 3; 
-    }
+    for (let j = 0; j < result.length; j++) {
 
-    let replay = 3;
-    if (j>= 1 && ) {
-        
-    }
-
-
-    for (i = i; i < numbers.length; i++) {
-        if (numbers[i] == 0 && numbers.length>1) {
-            numbers[i] = "";
-        }
-        if (i == 0) {
-            result[j][i] = wordNumbers0[numbers[i]];       
-        }else if (i == 1) {
-            if (numbers[i] + numbers[i-1] < 20) {
-                result[j][i-1] = wordNumbers0[numbers[i] + numbers[i-1]];
-                console.log(`number[i-1] ${result[i]}`);
-            }else{
-                result[j][i] = wordNumbers00[numbers[i]];
+        for (i = i; i < numbers.length; i++) {
+            if (numbers[i] == 0 && numbers.length>1) {
+                numbers[i] = "";
             }
-        }else if (i == 2) {
-            result[j][i] = wordNumbers000[numbers[i]];  
+            if (i == 0) {
+                result[j][i] = wordNumbers0[numbers[i]];       
+            }else if (i == 1) {
+                if (numbers[i] + numbers[i-1] < 20) {
+                    result[j][i-1] = wordNumbers0[numbers[i] + numbers[i-1]];
+                    console.log(`number[i-1] ${result[i]}`);
+                }else{
+                    result[j][i] = wordNumbers00[numbers[i]];
+                }
+            }else if (i == 2) {
+                result[j][i] = wordNumbers000[numbers[i]];  
+            }
         }
     }
-}
+
+
+
+
+
+
+
 // result[0].join(' ');
         // if (sum >= 20) {
         //     result[0] = wordNumbers00[numbers[0]];
@@ -56,5 +69,5 @@ for (let j = 0; j < Math.ceil(numbers.length/3); j++) {
         //     result[0] = wordNumbers0[numbers[0]];
         // }
 
-    document.getElementById("idWords").innerHTML = result[0].reverse().join(" ") + " Грн.";
+    document.getElementById("idWords").innerHTML = result.join(" ") + " Грн.";
 }
